bec2b978a3b63be2975fe59d446b670286bb3bfc
youpi0214 <ouambay@gmail.com>
2024-02-07 16:56:06 -0500

    refactor(MapView): switch from Options to composition API

--- src/components/restaurant_view/MapView.vue
+++ src/components/restaurant_view/MapView.vue
@@ -7,34 +7,62 @@
   </div>
 </template>
 
-<script>
+<script setup>
 const key = "AIzaSyC-TIo4u7jtVVu0yLHFe5hIdh3xICwIMmk";
-export default {
-  name: "MapView",
-  props: {
-    restaurantLocation: {
-      type: Object,
-      required: true,
-      // TODO this default will be removed once we'll dynamically get the location from the restaurant
-      default: () => {
-        return { lat: 46.782878601986255, lng: -71.28690361946938 };
-      },
-    },
-    zoom: {
-      type: Number,
-      default: 15,
-    },
-    mapTypeId: {
-      type: String,
-      default: "terrain",
-    },
-    buttonText: {
-      type: String,
-      default: "GET DIRECTIONS",
+const props = defineProps({
+  restaurantLocation: {
+    type: Object,
+    required: true,
+    // TODO this default will be removed once we'll dynamically get the location from the restaurant
+    default: () => {
+      return { lat: 46.782878601986255, lng: -71.28690361946938 };
     },
   },
-};
+  zoom: {
+    type: Number,
+    default: 15,
+  },
+  mapTypeId: {
+    type: String,
+    default: "terrain",
+  },
+  buttonText: {
+    type: String,
+    default: "GET DIRECTIONS",
+  },
+});
+
+function getDirections() {}
 </script>
+
+<!--<script>-->
+<!--const key = "AIzaSyC-TIo4u7jtVVu0yLHFe5hIdh3xICwIMmk";-->
+<!--export default {-->
+<!--  name: "MapView",-->
+<!--  props: {-->
+<!--    restaurantLocation: {-->
+<!--      type: Object,-->
+<!--      required: true,-->
+<!--      // TODO this default will be removed once we'll dynamically get the location from the restaurant-->
+<!--      default: () => {-->
+<!--        return { lat: 46.782878601986255, lng: -71.28690361946938 };-->
+<!--      },-->
+<!--    },-->
+<!--    zoom: {-->
+<!--      type: Number,-->
+<!--      default: 15,-->
+<!--    },-->
+<!--    mapTypeId: {-->
+<!--      type: String,-->
+<!--      default: "terrain",-->
+<!--    },-->
+<!--    buttonText: {-->
+<!--      type: String,-->
+<!--      default: "GET DIRECTIONS",-->
+<!--    },-->
+<!--  },-->
+<!--};-->
+<!--</script>-->
 
 <style scoped>
 .map-view {
