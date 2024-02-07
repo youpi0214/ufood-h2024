6f9b7ceffa019da168e9e74e1cf23846d39ac66d
youpi0214 <ouambay@gmail.com>
2024-02-07 17:52:04 -0500

    refactor(MapView): switch from Options to composition API

--- src/components/restaurant_view/MapView.vue
+++ src/components/restaurant_view/MapView.vue
@@ -32,37 +32,39 @@ const props = defineProps({
   },
 });
 
-function getDirections() {}
+function getDirections() {
+  navigator.geolocation.getCurrentPosition();
+}
 </script>
 
-<!--<script>-->
-<!--const key = "AIzaSyC-TIo4u7jtVVu0yLHFe5hIdh3xICwIMmk";-->
-<!--export default {-->
-<!--  name: "MapView",-->
-<!--  props: {-->
-<!--    restaurantLocation: {-->
-<!--      type: Object,-->
-<!--      required: true,-->
-<!--      // TODO this default will be removed once we'll dynamically get the location from the restaurant-->
-<!--      default: () => {-->
-<!--        return { lat: 46.782878601986255, lng: -71.28690361946938 };-->
-<!--      },-->
-<!--    },-->
-<!--    zoom: {-->
-<!--      type: Number,-->
-<!--      default: 15,-->
-<!--    },-->
-<!--    mapTypeId: {-->
-<!--      type: String,-->
-<!--      default: "terrain",-->
-<!--    },-->
-<!--    buttonText: {-->
-<!--      type: String,-->
-<!--      default: "GET DIRECTIONS",-->
-<!--    },-->
-<!--  },-->
-<!--};-->
-<!--</script>-->
+<script>
+const key = "AIzaSyC-TIo4u7jtVVu0yLHFe5hIdh3xICwIMmk";
+export default {
+  name: "MapView",
+  props: {
+    restaurantLocation: {
+      type: Object,
+      required: true,
+      // TODO this default will be removed once we'll dynamically get the location from the restaurant
+      default: () => {
+        return { lat: 46.782878601986255, lng: -71.28690361946938 };
+      },
+    },
+    zoom: {
+      type: Number,
+      default: 15,
+    },
+    mapTypeId: {
+      type: String,
+      default: "terrain",
+    },
+    buttonText: {
+      type: String,
+      default: "GET DIRECTIONS",
+    },
+  },
+};
+</script>
 
 <style scoped>
 .map-view {
