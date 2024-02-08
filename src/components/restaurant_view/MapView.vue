49bbb989ea7dad4b093557d7540bcc114618338f
youpi0214 <ouambay@gmail.com>
2024-02-07 22:24:00 -0500

    refactor(MapView): switch back from composition to Options API

--- src/components/restaurant_view/MapView.vue
+++ src/components/restaurant_view/MapView.vue
@@ -7,36 +7,6 @@
   </div>
 </template>
 
-<script setup>
-const key = "AIzaSyC-TIo4u7jtVVu0yLHFe5hIdh3xICwIMmk";
-const props = defineProps({
-  restaurantLocation: {
-    type: Object,
-    required: true,
-    // TODO this default will be removed once we'll dynamically get the location from the restaurant
-    default: () => {
-      return { lat: 46.782878601986255, lng: -71.28690361946938 };
-    },
-  },
-  zoom: {
-    type: Number,
-    default: 15,
-  },
-  mapTypeId: {
-    type: String,
-    default: "terrain",
-  },
-  buttonText: {
-    type: String,
-    default: "GET DIRECTIONS",
-  },
-});
-
-function getDirections() {
-  navigator.geolocation.getCurrentPosition();
-}
-</script>
-
 <script>
 const key = "AIzaSyC-TIo4u7jtVVu0yLHFe5hIdh3xICwIMmk";
 export default {
