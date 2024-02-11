207061192a3095fe2876c6740cb0246e77415cda
youpi0214 <ouambay@gmail.com>
2024-02-11 17:28:53 -0500

    feat: swtich from google api to mapbox

--- src/components/restaurant_view/MapView.vue
+++ src/components/restaurant_view/MapView.vue
@@ -1,79 +1,96 @@
 <template>
-  <div class="">
-    <div class="">
-      <GMapMap
-        ref="map-view"
-        :center="restaurantLocation"
-        :zoom="zoom"
-        :map-type-id="mapTypeId"
-      >
-        <GMapMarker :position="restaurantLocation" />
-        <GMapMarker
-          :position="currentPosition"
-          icon="https://maps.google.com/mapfiles/ms/icons/blue-dot.png"
-        />
-      </GMapMap>
-    </div>
-    <button type="button" class="btn btn-primary" @click="getRoute">
-      {{ buttonText }}
-    </button>
+  <div>
+    <div ref="map" style="height: 400px;"></div>
   </div>
 </template>
+
 <script>
+import mapboxgl from '!mapbox-gl';
+
 export default {
-  name: "App",
-  props: {
-    zoom: {
-      type: Number,
-      default: 15,
-    },
-    mapTypeId: {
-      type: String,
-      default: "terrain",
-    },
-    buttonText: {
-      type: String,
-      default: "GET DIRECTIONS",
-    },
-  },
-  setup() {
-    return { mapView: null };
-  },
   data() {
     return {
-      restaurantLocation: { lat: 46.782878601986255, lng: -71.28690361946938 },
+      map: null,
       currentPosition: null,
-      routeCoordinates: null,
+      restaurantLocation: [ 46.782878601986255, -71.28690361946938 ],
     };
   },
   mounted() {
-    this.$nextTick(() => {
-      this.mapView = this.$refs["map-view"];
-    });
-  },
-  created() {
     this.getLocation();
+    this.initMap();
   },
   methods: {
+    initMap() {
+      mapboxgl.accessToken = 'pk.eyJ1IjoieW91cGkwMjE0IiwiYSI6ImNsc2kxZWkxNjFhdHoydnFwbWtvemFrOHIifQ.Pil0AJAwK_TVItQJAWkb9g';
+      this.map = new mapboxgl.Map({
+        container: this.$refs.map,
+        style: 'mapbox://styles/mapbox/streets-v11', // You can choose a different map style
+        center: this.restaurantLocation,
+        zoom: 8,
+      });
+
+      this.addRoute();
+    },
     getLocation() {
       if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition((position) => {
-          this.currentPosition = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-          };
-        });
-      } else alert("Geolocation is not supported by this browser.");
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            this.currentPosition = [position.coords.longitude, position.coords.latitude]; // [lng, lat
+          },
+          (error) => {
+            console.error("Error getting current location:", error);
+          },
+          { enableHighAccuracy: true, timeout: 5000, maximumAge: 0 },
+        );
+      } else {
+        alert("Geolocation is not supported by this browser.");
+      }
     },
-    getRoute() {
-      console.log("Getting route...");
+    addRoute() {
+      const originLngLat = this.currentPosition;
+      const destinationLngLat = this.restaurantLocation;
+
+      const coordinates = [originLngLat, destinationLngLat];
+
+      this.map.on('load', () => {
+        this.map.addLayer({
+          id: 'route',
+          type: 'line',
+          source: {
+            type: 'geojson',
+            data: {
+              type: 'Feature',
+              properties: {},
+              geometry: {
+                type: 'LineString',
+                coordinates: coordinates,
+              },
+            },
+          },
+          layout: {
+            'line-join': 'round',
+            'line-cap': 'round',
+          },
+          paint: {
+            'line-color': '#888',
+            'line-width': 8,
+          },
+        });
+        this.map.addControl(
+          new mapboxgl.GeolocateControl({
+            positionOptions: {
+              enableHighAccuracy: true
+            },
+            trackUserLocation: true,
+            showUserHeading: true
+          }))
+      });
+
     },
   },
 };
 </script>
 
 <style scoped>
-button:hover {
-  cursor: pointer;
-}
+/* Add your custom styles here */
 </style>
