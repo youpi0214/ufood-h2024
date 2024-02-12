4284ece778d711393c7b1e08dc26b4384ee400f6
youpi0214 <ouambay@gmail.com>
2024-02-11 22:57:20 -0500

    fix: repaired the master branch

---
+++ src/components/RecentlyVisited.vue
@@ -0,0 +1,64 @@
+<script>
+import RestaurantCards from "./HomeOrg/RestaurantCards.vue";
+
+export default {
+  name: "RecentlyVisited",
+  data() {
+    return {
+      restaurantArray: [
+        {
+          name: "Kebab House",
+          Visits: 3,
+        },
+        {
+          name: "Breakfast Spot",
+          Visits: 2,
+        },
+        {
+          name: "POKI",
+          Visits: 1,
+        },
+        {
+          name: "Papa's Pizzeria",
+          Visits: 4,
+        },
+        {
+          name: "Dessert House",
+          Visits: 5,
+        },
+        {
+          name: "Sushi Bar",
+          Visits: 6,
+        },
+        {
+          name: "Burger House",
+          Visits: 7,
+        },
+        {
+          name: "Pasta House",
+          Visits: 8,
+        },
+        {
+          name: "American Diner",
+          Visits: 9,
+        },
+        {
+          name: "Lebanese House",
+          Visits: 10,
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<template>
+  <div>
+    <h1>Recently Visited</h1>
+    <div v-for="restaurant in restaurantArray" :key="restaurant.name">
+      <p>{{ restaurant.name }} - {{ restaurant.Visits }} Visits</p>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/restaurant_view/PageHeaderInfos.vue
@@ -0,0 +1,105 @@
+<script>
+export default {
+  name: "PageHeaderInfos",
+  props: {
+    name: {
+      type: String,
+      default: "McDonald's",
+    },
+    rating: {
+      type: Number,
+      default: 4.8,
+    },
+    address: {
+      type: String,
+      default: "⚲ 2520 Chem. des Quatre-Bourgeois, QC, G1V 4R2",
+    },
+    tel: {
+      type: String,
+      default: "(418) 651-5085",
+    },
+    genres: {
+      type: String,
+      default: "Fast-Food",
+    },
+    price_range: {
+      type: String,
+      default: "$$",
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="container">
+    <div class="left-half">
+      <div>
+        <span id="title">{{ name }}</span>
+        <span id="rating">{{ "★" }} {{ rating }}</span>
+      </div>
+      <div>{{ address }}</div>
+    </div>
+    <div class="right-half">
+      <div>{{ tel }}</div>
+      <div>{{ genres }} {{ "•" }} {{ price_range }}</div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 200px;
+}
+
+.left-half,
+.right-half {
+  width: calc(50% - 5px);
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  padding: 10px;
+}
+
+.right-half {
+  align-items: flex-end;
+}
+
+#title {
+  font-size: 2rem;
+  margin-top: 15px;
+  font-weight: bold;
+}
+
+#rating {
+  font-size: 1rem;
+  padding: 5px;
+  border-radius: 20px;
+  margin-left: 10px;
+  color: #000000;
+}
+
+@media (max-width: 600px) {
+  /* Media query for mobile screens */
+  .container {
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .left-half,
+  .right-half {
+    width: 100%;
+    height: auto;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+  }
+}
+</style>
