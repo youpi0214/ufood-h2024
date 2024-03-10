c571f140c4af35e451486193cf37c1dc53d4ce40
Ahmed Sami <ahsam02@ulaval.ca>
2024-03-10 16:21:04 -0400

    refactor: optimizing map performance minor carousel interface change

--- src/components/restaurantView/ImageSlider.vue
+++ src/components/restaurantView/ImageSlider.vue
@@ -10,40 +10,60 @@ export default {
         "https://images4.alphacoders.com/109/thumb-1920-1093163.jpg",
       ],
     },
+    currentIndex: {
+      type: Number,
+      default: 0,
+    }
   },
+  methods: {
+    prevImage() {
+      this.$emit('update:currentIndex', (this.currentIndex + 1) % this.pictures.length);
+    },
+    nextImage() {
+      this.$emit('update:currentIndex', (this.currentIndex - 1 + this.pictures.length) % this.pictures.length);
+    }
+  }
 };
 </script>
 
 <template>
-  <div id="carouselExample" class="carousel slide">
+  <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
     <div class="carousel-inner">
       <div
         v-for="(picture, index) in pictures"
         :key="index"
-        :class="['carousel-item', { active: index === 0 }]"
+        :class="['carousel-item', { active: index === this.currentIndex}]"
       >
         <img :src="picture" class="d-block w-100" alt="..." />
       </div>
     </div>
     <button
+      id="buttonSlide"
       class="carousel-control-prev"
       type="button"
-      data-bs-target="#carouselExample"
+      data-bs-target="#carouselExampleControls"
       data-bs-slide="prev"
+      @click="prevImage"
     >
       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span>
     </button>
     <button
+      id="buttonSlide"
       class="carousel-control-next"
       type="button"
-      data-bs-target="#carouselExample"
+      data-bs-target="#carouselExampleControls"
       data-bs-slide="next"
+      @click="nextImage"
     >
       <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
     </button>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+#buttonSlide {
+  background: transparent;
+  border: none;
+}
+</style>
+
