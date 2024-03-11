e35f2250861ddb1abb1e2245b294c5d0ea3303ba
Nasma Chaoui <nasma.chaoui.1@ulaval.ca>
2024-03-10 23:42:34 -0400

    refactor: added all Restaurants elements

--- src/components/homeView/script/card.utility.js
+++ src/components/homeView/script/card.utility.js
@@ -1,7 +1,11 @@
 export class Restaurant {
-  constructor({ name, id, pictures, genres, price_range, rating }) {
+  constructor({ name, id, address, tel, location, opening_hours, pictures, genres, price_range, rating }) {
     this.name = name;
     this.id = id;
+    this.address = address;
+    this.tel = tel;
+    this.location = location;
+    this.openingHours = opening_hours;
     this.pictures = pictures;
     this.genres = genres;
     this.priceRange = price_range;
