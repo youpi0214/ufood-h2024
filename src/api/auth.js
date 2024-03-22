a0552054e20ffe67b1dbab6a052adb56dd1e3dcd
youpi0214 <ouambay@gmail.com>
2024-03-22 13:20:00 -0400

    feat: add user authentication api
    needs to be implemented with the ui now

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,39 +1,54 @@
-const BASE_URL = "https://ufoodapi.herokuapp.com//unsecure"; // Assuming your API base URL
+import { BASE_URL } from "./api.utility.js";
 
 export const login = async (email, password) => {
-  const response = await fetch(`${BASE_URL}/login`, {
+  return fetch(`${BASE_URL}/login`, {
     method: "POST",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-    body: `email=${encodeURIComponent(email)}&password=${encodeURIComponent(password)}`,
-  });
-  return await response.json();
+    body: new URLSearchParams({ email: email, password: password }),
+  })
+    .then((response) => {
+      if (!response.ok) throw new Error("Failed to login");
+
+      return response.json();
+    })
+    .then((user) => user) // attributes: email, name, token, id, followers [], following [], rating
+    .catch((error) => console.error(error));
 };
 
 export const logout = async () => {
-  const response = await fetch(`${BASE_URL}/logout`, {
+  return fetch(`${BASE_URL}/logout`, {
     method: "POST",
-  });
-  return await response.json();
+  })
+    .then((response) => {
+      if (!response.ok) throw new Error("Failed to logout");
+      else return "Successfully logged out";
+    })
+    .catch((error) => console.error(error));
 };
 
 export const signup = async (name, email, password) => {
-  const response = await fetch(`${BASE_URL}/signup`, {
+  return fetch(`${BASE_URL}/signup`, {
     method: "POST",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-    body: `name=${encodeURIComponent(name)}&email=${encodeURIComponent(email)}&password=${encodeURIComponent(password)}`,
-  });
-  return await response.json();
+    body: new URLSearchParams({ name: name, email: email, password: password }),
+  })
+    .then((response) => {
+      if (!response.ok) throw new Error("Failed to signup");
+
+      return response.json();
+    })
+    .then((newUser) => newUser) // attributes: email, name, id, followers [], following [], rating
+    .catch((error) => console.error(error));
 };
 
 export const getTokenInfo = async (token) => {
-  const response = await fetch(`${BASE_URL}/tokenInfo`, {
-    headers: {
-      Authorization: `Token ${token}`,
-    },
-  });
-  return await response.json();
+  return fetch(`${BASE_URL}/tokenInfo`, {
+    method: "GET",
+    headers: { Authorization: token },
+  })
+    .then((response) => {
+      if (!response.ok) throw new Error("Failed to get token info");
+
+      return response.json();
+    })
+    .then((userLinkedToToken) => userLinkedToToken) // attributes: email, name, token, id, followers [], following [], rating
+    .catch((error) => console.error(error));
 };
