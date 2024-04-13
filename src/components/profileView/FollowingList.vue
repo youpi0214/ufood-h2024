86f4fa5e8d6ae9643951eb268e63ddac9d15a032
saelf9 <saelf9@ulaval.ca>
2024-04-13 16:14:19 -0400

    feature: ui of follower and following

---
+++ src/components/profileView/FollowingList.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="followingItem in following" :key="followingItem.id">
+        <img :src="followingItem.avatarUrl" alt="Following Avatar">
+        <span class="username">{{ followingItem.username }}</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    following: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  padding: 0;
+}
+
+li {
+  display: flex;
+  align-items: center;
+  margin-bottom: 1rem;
+}
+
+img {
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  margin-right: 1rem;
+}
+
+.username {
+  font-weight: bold;
+  font-size: 1.2rem;
+}
+</style>
