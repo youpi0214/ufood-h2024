34b3fa99ede004ed2f7fb349135c9e77eacbb061
Yannick Andre Ouamba <yannick-andre.ouamba.1@ulaval.ca>
2024-04-17 23:05:32 -0400

    feat: modal popup

--- src/router/router.js
+++ src/router/router.js
@@ -4,6 +4,8 @@ import Restaurant from "@/components/Restaurant";
 import User from "@/components/User";
 import Authentication from "@/components/Authentication.vue";
 import Cookies from "js-cookie";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Modal } from "bootstrap";
 
 const routes = [
   {
@@ -35,8 +37,36 @@ export const router = createRouter({
 
 router.beforeEach((to, from, next) => {
   if (to.name !== "Authentication" && !Cookies.get("token")) {
+    displayPopup("Please Login", "You need to be authenticated to access this page.");
+
     next({ name: "Authentication" });
   } else {
     next();
   }
 });
+
+
+function displayPopup(title, message) {
+  const modal = document.createElement("div");
+  modal.classList.add("modal", "fade", "show");
+  modal.setAttribute("role", "dialog");
+  modal.innerHTML = `
+    <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">${title}</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">${message}</div>
+      </div>
+    </div>
+  `;
+  document.body.appendChild(modal);
+  const modalInstance = new Modal(modal);
+  modalInstance.show();
+  setTimeout(() => {
+    modalInstance.hide();
+    document.body.removeChild(modal);
+  }, 4000);
+}
+
