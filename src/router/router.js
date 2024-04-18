d92a716719440ff06e7a2b7b6e7756eb6499d9ce
Yannick Andre Ouamba <yannick-andre.ouamba.1@ulaval.ca>
2024-04-17 20:17:56 -0400

    feat: prevent access to anything if not logged in

--- src/router/router.js
+++ src/router/router.js
@@ -3,32 +3,40 @@ import Home from "@/components/Home";
 import Restaurant from "@/components/Restaurant";
 import User from "@/components/User";
 import Authentication from "@/components/Authentication.vue";
+import Cookies from "js-cookie";
 
 const routes = [
   {
     path: "/",
     name: "Home",
-    component: Home,
+    component: Home
   },
   {
     path: "/restaurants/:restaurantId",
     name: "Restaurant",
-    component: Restaurant,
-    props: true,
+    component: Restaurant
   },
   {
     path: "/user",
     name: "User",
-    component: User,
+    component: User
   },
   {
     path: "/auth",
     name: "Authentication",
-    component: Authentication,
-  },
+    component: Authentication
+  }
 ];
 
 export const router = createRouter({
   history: createWebHashHistory(),
-  routes,
+  routes
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.name !== "Authentication" && !Cookies.get("token")) {
+    next({ name: "Authentication" });
+  } else {
+    next();
+  }
 });
