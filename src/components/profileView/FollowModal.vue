a7eed85a5a4ce4d77d6591195f8ce44f9424598d
Ahmed Sami <ahmed.sami.1@ulaval.ca>
2024-04-19 18:15:07 -0400

    design: following modal

--- src/components/profileView/FollowModal.vue
+++ src/components/profileView/FollowModal.vue
@@ -6,13 +6,13 @@
 
     <!-- Modal -->
     <div
-      class="modal modal-dialog-scrollable"
+      class="modal"
       :id="modalId"
       tabindex="-1"
       :aria-labelledby="`follow/${modalId}`"
       aria-hidden="true"
     >
-      <div class="modal-dialog modal-dialog-scrollable">
+      <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered">
         <div class="modal-content">
           <div class="modal-header">
             <h1 class="modal-title fs-5" :id="`follow/${modalId}`">
@@ -28,10 +28,16 @@
           <div class="modal-body">
             <div v-for="follower in this.following" :key="follower.id">
               <div
+                id="follower"
                 @click="this.$router.push(`/user/${follower.id}`)"
                 data-bs-dismiss="modal"
               >
-                <span>{{ follower.name }}</span>
+                <img
+                  :src="gravatarUrl(follower.email)"
+                  :alt="follower.name"
+                  style="flex: 1; width: 50%; padding: 1rem; border-radius: 5rem"
+                />
+                <div style="flex: 10; padding: 1rem; display: flex; align-items: center">{{ follower.name }}</div>
               </div>
             </div>
           </div>
@@ -42,6 +48,8 @@
 </template>
 
 <script>
+import md5 from "md5";
+
 export default {
   name: "FollowModal",
   props: {
@@ -59,7 +67,20 @@ export default {
       divModalId: "#" + this.modalId,
     };
   },
+  methods: {
+    gravatarUrl(email) {
+      return `https://www.gravatar.com/avatar/${this.md5Email(email)}?s=80&d=identicon`;
+    },
+    md5Email(email) {
+      return md5(email.trim().toLowerCase());
+    },
+  },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+#follower {
+  display: flex;
+  flex-direction: row;
+}
+</style>
